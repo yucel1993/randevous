a459e6620d260c844ea417e2fae3c1f9bd7b05bd
Burhan <alpagut1993@gmail.com>
2024-07-06 19:39:25 +0200

    appointment:prepared

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique,
+      ab? Beatae sit unde tempore quia ipsum ipsa voluptate facilis omnis
+      ducimus! Incidunt corporis aliquam distinctio, laborum earum, ipsa
+      temporibus deserunt quis quibusdam pariatur accusantium!
+    </div>
+  );
 };
 
 export default page;

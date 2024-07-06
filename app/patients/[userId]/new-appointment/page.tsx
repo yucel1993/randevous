c4301391f963c2e5f50be856d1116b479c9f2b3a
Burhan <alpagut1993@gmail.com>
2024-07-06 20:54:01 +0200

    passkeyModal

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import Image from "next/image";
 
-const page = () => {
+import { getPatient } from "@/lib/actions/patient.actions";
+import { AppointmentForm } from "@/components/forms/AppointmentForm";
+
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
   return (
-    <div>
-      page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique,
-      ab? Beatae sit unde tempore quia ipsum ipsa voluptate facilis omnis
-      ducimus! Incidunt corporis aliquam distinctio, laborum earum, ipsa
-      temporibus deserunt quis quibusdam pariatur accusantium!
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create"
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
     </div>
   );
 };
 
-export default page;
+export default Appointment;
